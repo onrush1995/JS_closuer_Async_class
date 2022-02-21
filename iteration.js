20a004908edf573a531f81a63fca117e0c0cceac
Moni <moniruzzaman.eng1995@gmail.com>
2022-02-21 16:28:09 +0200

    filter a number with filter function

--- iteration.js
+++ iteration.js
@@ -1,5 +1,9 @@
 const names = ["Selma","Ted","Mike","Sam","Sharon","Marvin"];
-
-const sNames = names.filter( name => name.charAt(0)==="S");
+const startWithS = name => name.charAt(0)==="S";
+const sNames = names.filter(startWithS);
 
 console.log(sNames);
+
+const numbers = [1,2,3,4,5,6,0];
+const no3 = numbers.filter(number => number==2);
+console.log(no3);
